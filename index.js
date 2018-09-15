d92f15fcb3671d3baa00fb60fc11f5b9c7822cd3
Ubuntu <ubuntu@ip-172-31-5-188.us-west-2.compute.internal>
2018-09-15 15:51:19 +0000

    remove socket experiment

--- index.js
+++ index.js
@@ -28,8 +28,3 @@ app.get('/radiou', function(req, res) {
 	res.sendFile(__dirname + '/views/radiou.html');
 });
 
-app.post('/new_activity', function(req, res) {
-	console.log(req.query);
-	console.log(req.body);
-	res.status(200).json({"message": "Success"});
-});
